4882aaa389a70171b73c8c5c2f21ef93bc4976f9
Ivan Mix <imix@MIX-ZEPHYR>
2025-02-22 06:30:26 -0500

    Eliminated id from both users and groups sql structures

--- postgres/static.init.sql
+++ postgres/static.init.sql
@@ -1,14 +1,12 @@
 CREATE TYPE clearance_level AS ENUM ('UNCLASSIFIED', 'CUI', 'SECRET', 'TOPSECRET');
 
 CREATE TABLE IF NOT EXISTS users (
-    id SERIAL PRIMARY KEY,
     username VARCHAR(255) UNIQUE NOT NULL,
     password_hash VARCHAR(255) NOT NULL,
-    group_ids SERIAL[] DEFAULT ARRAY[]::SERIAL[]
+    group_names VARCHAR(255)[] DEFAULT ARRAY[]::VARCHAR[]
 );
 
 CREATE TABLE IF NOT EXISTS groups (
-    id SERIAL PRIMARY KEY,
     group_name VARCHAR(255) UNIQUE NOT NULL,
     password_hash VARCHAR(255) NOT NULL,
     aes_key VARCHAR(255) NOT NULL,
